e3916a9ead5ddacc70eb3c7c43116eb52a329d63
sruthibijili <ee24btech11060@iith.ac.in>
2025-01-05 11:16:40 +0530

    ee24btech11060

---
+++ ncert/12/9/5/8/codes/code.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import math
+from ctypes import *
+
+#loading the shared obect file
+funcs = CDLL('./funcs.so')
+
+#assigning the C functions in Python
+x_func = funcs.x
+y_func = funcs.y
+y_act = funcs.y_val
+
+#setting the result type of the C-Python functions
+x_func.restype = c_float
+y_func.restype = c_float
+y_act.restype = c_float
+
+#setting initial coordinates as the point given in the question
+x = 1.0
+y = 1.57
+plt.plot(x,y,'ro', label='Simulation Plot')
+plt.plot(x,y,'y*', label='Theoretical Plot')
+
+#initialising h
+h = 0.01
+
+#iterating to generate more points on the curve using finite differences and plotting each indivdual point
+for i in range(100):
+    y = y_func(c_float(x), c_float(y), c_float(h)) #y-coordinate of simulation plot
+    y_val = y_act(c_float(x)) #y-coordinate of theoretical plot
+    x = x_func(c_float(x), c_float(h)) #x-coordinate of simulation plot and theoretical plot
+    plt.plot(x,y,'ro') #plotting simulation curve
+    plt.plot(x,y_val,'y*') #plotting theoretical curve
+
+#setting up the labels and grid for the plot
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.grid(True)
+
+#adding a legend
+plt.legend()
+
+#saving the generated plot
+plt.savefig('../figs/fig.pdf')
